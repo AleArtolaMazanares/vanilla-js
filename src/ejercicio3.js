528f1d4eb4f5107b3db073932db0d28a8e0f8fe7
Alejandro Artola <aartolam@fwdcostarica.com>
2023-06-21 11:53:02 -0600

    guardado

--- src/ejercicio3.js
+++ src/ejercicio3.js
@@ -1,6 +1,40 @@
-// Inserte el código aquí
+// // Inserte el código aquí
+// let arreglo = [1, 2, 3, 4];
 
+function ClonarArreglo(arreglo) {
+  return (lista = [...arreglo]);
+}
 
+let areglo1 = [1, 2, 3, 4];
+ClonarArreglo(areglo1);
+
+function ClonarObjeto(objeto) {
+  return (lista = { ...objeto });
+}
+
+let casa = {
+  techo: "rojo",
+  verde: "verde",
+};
+
+ClonarObjeto(casa);
+
+// tres
+function SumaTotal(valor1, valor2) {
+  return (sumar = valor1 + valor2);
+}
+let sumaActual = [1, 2, 4, 5].reduce(SumaTotal, 0);
+console.log(sumaActual);
+// tres
+
+// cuatro
+
+function Saludar(nombre) {
+  return (lista = nombre + saludo);
+}
+let nombre = ["samuel "];
+let saludo = ["hola como estas?"];
+Saludar(nombre);
 
 // NO MODIFICAR
 export { clonarArreglo, clonarObjeto, sumar, saludar };
